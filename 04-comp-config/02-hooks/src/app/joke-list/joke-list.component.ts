a4cac9c57eefb34b26f612a9863d168cc88920c1
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2017-10-30 11:48:15 -0400

    updated hooks example

--- 04-comp-config/02-hooks/src/app/joke-list/joke-list.component.ts
+++ 04-comp-config/02-hooks/src/app/joke-list/joke-list.component.ts
@@ -7,26 +7,36 @@ import { Joke } from '../joke';
   styleUrls: ['./joke-list.component.css']
 })
 export class JokeListComponent implements OnInit {
-  jokes: Joke[];
+  jokes: Joke[] = [];
 
   constructor() {
-    this.jokes = [
-      new Joke("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
-      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
-      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
-    ];
+    // this.jokes = [
+    //   new Joke("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
+    //   new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
+    //   new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
+    // ];
   }
 
-  addJoke(joke) {
-    this.jokes.unshift(joke);
+  // addJoke(joke) {
+  //   this.jokes.unshift(joke);
+  // }
+
+  addJoke() {
+    this.jokes.unshift(new Joke("What did the cheese say when it looked in the mirror",
+      "Hello-me (Helloumi)"));
   }
 
-  deleteJoke(joke) {
-    let indexToDelete = this.jokes.indexOf(joke);
-    if (indexToDelete !== -1) {
-      this.jokes.splice(indexToDelete,1);
-    }
+  // deleteJoke(joke) {
+  //   let indexToDelete = this.jokes.indexOf(joke);
+  //   if (indexToDelete !== -1) {
+  //     this.jokes.splice(indexToDelete,1);
+  //   }
+  // }
+
+  deleteJoke() {
+    this.jokes = [];
   }
+
   ngOnInit() {
   }
 }
