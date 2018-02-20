e9fc9857075e9bf799ee090663df871d8ef635ee
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2018-02-20 14:55:11 -0500

    card hover directive

--- 07-directives/08-custom-directive/cardhoover-app/src/app/card-hoover.directive.ts
+++ 07-directives/08-custom-directive/cardhoover-app/src/app/card-hoover.directive.ts
@@ -9,6 +9,7 @@ import {
 export class CardHooverDirective {
 
   constructor(private el: ElementRef, private renderer: Renderer) {
+    // el.nativeElement.style.backgroundColor = "gray";
     renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
   }
 
