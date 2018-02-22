78e1136caaa4900357f7f714001de9380e65eeda
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2018-02-22 11:30:59 -0500

    init model-form-validation app

---
+++ 10-forms/model-form-validation-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ModelFormComponent } from './model-form/model-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ModelFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
