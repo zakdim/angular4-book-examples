c80a0e4456eb8e26cfc895f9526293cabea58420
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2018-02-21 23:35:22 -0500

    model-form example

--- 10-forms/model-form-app/src/app/app.module.ts
+++ 10-forms/model-form-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ModelFormComponent } from './model-form/model-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelFormComponent
   ],
   imports: [
     BrowserModule
---
+++ 10-forms/model-form-app/src/app/model-form/model-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
+@Component({
+  selector: 'model-form',
+  templateUrl: './model-form.component.html',
+  styleUrls: ['./model-form.component.css']
+})
+export class ModelFormComponent implements OnInit {
+  langs: string[] = [
+    'English', 
+    'French', 
+    'German',
+  ];
+  myform: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.myform = new FormGroup({
+      name: new FormGroup({
+        firstName: new FormControl(),
+        lastName: new FormControl(),
+      }),
+      email: new FormControl(),
+      password: new FormControl(),
+      language: new FormControl()
+    });
+  }
+}
